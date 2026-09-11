ddc454b0616938b43542e8b92cafd910f42356b1
yjk1101 <92ujin@naver.com>
2026-09-11 17:19:10 +0900

    20260911:question,answer

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -18,8 +18,10 @@ def create_app():
     from . import models
 
     # 블루프린트 등록
-    from .views import main_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
 
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,12 +1,8 @@
-from flask import Blueprint, render_template
-
-from pybo.models import Question
+from flask import Blueprint, redirect, url_for
 
 # Blueprint: 라우팅 함수를 체계적으로 관리
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc()).all()
-
-    return render_template('question/question_list.html', question_list=question_list)
+    return redirect(url_for('question._list'))
