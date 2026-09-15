fff0bcfa564ae48139ef2fde4563edf4c5c06fdc
yjk1101 <92ujin@naver.com>
2026-09-15 13:18:15 +0900

    20260915:게시판,페이징

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Blueprint, redirect, request, url_for
+from flask import Blueprint, redirect, request, url_for, render_template
 
+from pybo.forms import AnswerForm
 from pybo import db
 from pybo.models import Question, Answer
 
@@ -9,12 +10,16 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods = ['POST'])
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
+    if form.validate_on_submit():
+        # content = request.form['content']  # 폼태그에 입력된 값 받을 때
+        content = form.content.data  # 폼모듈로 입력된 값 받을 때
+        answer = Answer(question=question, content=content, create_date=datetime.now())
+        db.session.add(answer)
+        db.session.commit()
 
-    answer = Answer(question=question, content= content, create_date = datetime.now())
-    db.session.add(answer)
-    db.session.commit()
+        return redirect(url_for('question.detail', question_id = question_id))
 
-    return redirect(url_for('question.detail', question_id = question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
 
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Blueprint, render_template, request, redirect, url_for
 
 from pybo import db
-from pybo.forms import QuestionForm
+from pybo.forms import QuestionForm, AnswerForm
 from pybo.models import Question
 
 # Blueprint: 라우팅 함수를 체계적으로 관리
@@ -11,15 +11,18 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list')
 def _list():
-    question_list = Question.query.order_by(Question.create_date.desc()).all()
+    page = request.args.get('page', default=1, type=int)  # url parameter?page=1 이런식으로 보여짐
+    question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)  # 페이징 구현
 
     return render_template('question/question_list.html', question_list=question_list)
 
 @bp.route('/detail/<int:question_id>')
 def detail(question_id):
+    form = AnswerForm()  # 질문 상세 템플릿에 폼 추가
     question = Question.query.get_or_404(question_id)
 
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
@@ -28,5 +31,7 @@ def create():
         question = Question(subject= form.subject.data, content= form.content.data, create_date= datetime.now())
         db.session.add(question)
         db.session.commit()
+
         return redirect(url_for('main.index'))
+
     return render_template('question/question_form.html', form=form)
